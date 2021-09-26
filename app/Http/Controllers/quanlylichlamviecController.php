22a61a4801aedda7f8cabf789709c404068316e7
buihuuchau <buihuuchau99@gmail.com>
2021-09-26 19:32:58 +0700

    Giao diện, ràng buộc lịch làm việc

--- app/Http/Controllers/quanlylichlamviecController.php
+++ app/Http/Controllers/quanlylichlamviecController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 // use App\Traits\StorageImageTrait;
-Use Alert;
+use Alert;
 use Illuminate\Support\Facades\Redirect;
 use Session;
 use Illuminate\Support\Str;
@@ -12,134 +13,140 @@ use Illuminate\Http\Request;
 
 class quanlylichlamviecController extends Controller
 {
-    public function quanlylichlamviec(){
+    public function quanlylichlamviec()
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
-        
+
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
         $khuvuc = DB::table('khuvuc')
-            ->where('idquan',$thanhvien->idquan)
-            ->where('hidden',0)
+            ->where('idquan', $thanhvien->idquan)
+            ->where('hidden', 0)
             ->get();
         $calam = DB::table('calam')
-            ->where('idquan',$thanhvien->idquan)
-            ->where('hidden',0)
+            ->where('idquan', $thanhvien->idquan)
+            ->where('hidden', 0)
             ->get();
         $lichlamviec = DB::table('lichlamviec')
             ->orderBy('hoten')
-            ->where('lichlamviec.idquan',$thanhvien->idquan)
-            ->where('thoigian',date('Y-m-d'))
-            ->join('thanhvien', 'lichlamviec.idthanhvien','=','thanhvien.id')
-            ->select('lichlamviec.*','thanhvien.hoten')
+            ->where('lichlamviec.idquan', $thanhvien->idquan)
+            ->where('thoigian', date('Y-m-d'))
+            ->join('thanhvien', 'lichlamviec.idthanhvien', '=', 'thanhvien.id')
+            ->select('lichlamviec.*', 'thanhvien.hoten')
             ->get();
         $date = date('Y-m-d');
-        return view('lichlamviec.quanlylichlamviec',compact('thanhvien','khuvuc','calam','lichlamviec','date'));
+        return view('lichlamviec.quanlylichlamviec', compact('thanhvien', 'khuvuc', 'calam', 'lichlamviec', 'date'));
     }
-    public function xemlichlamviec(Request $request){
+    public function xemlichlamviec(Request $request)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
         $date = $request->thoigian;
 
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
         $khuvuc = DB::table('khuvuc')
-            ->where('idquan',$thanhvien->idquan)
-            ->where('hidden',0)
+            ->where('idquan', $thanhvien->idquan)
+            ->where('hidden', 0)
             ->get();
         $calam = DB::table('calam')
-            ->where('idquan',$thanhvien->idquan)
-            ->where('hidden',0)
+            ->where('idquan', $thanhvien->idquan)
+            ->where('hidden', 0)
             ->get();
         $lichlamviec = DB::table('lichlamviec')
             ->orderBy('hoten')
-            ->where('lichlamviec.idquan',$thanhvien->idquan)
-            ->where('thoigian',$date)
-            ->join('thanhvien', 'lichlamviec.idthanhvien','=','thanhvien.id')
-            ->select('lichlamviec.*','thanhvien.hoten')
+            ->where('lichlamviec.idquan', $thanhvien->idquan)
+            ->where('thoigian', $date)
+            ->join('thanhvien', 'lichlamviec.idthanhvien', '=', 'thanhvien.id')
+            ->select('lichlamviec.*', 'thanhvien.hoten')
             ->get();
-        
-        return view('lichlamviec.quanlylichlamviec',compact('thanhvien','khuvuc','calam','lichlamviec','date'));
+
+        return view('lichlamviec.quanlylichlamviec', compact('thanhvien', 'khuvuc', 'calam', 'lichlamviec', 'date'));
     }
-    public function diemdanhcomatlichlamviec($id){
+    public function diemdanhcomatlichlamviec($id)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
-        
+
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
         $lichlamviec['diemdanh'] = 1;
         DB::table('lichlamviec')
-            ->where('id',$id)
-            ->where('idquan',$thanhvien->idquan)
+            ->where('id', $id)
+            ->where('idquan', $thanhvien->idquan)
             ->update($lichlamviec);
         return back();
     }
-    public function diemdanhvangmatlichlamviec($id){
+    public function diemdanhvangmatlichlamviec($id)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
-        
+
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
         $lichlamviec['diemdanh'] = 0;
         DB::table('lichlamviec')
-            ->where('id',$id)
-            ->where('idquan',$thanhvien->idquan)
+            ->where('id', $id)
+            ->where('idquan', $thanhvien->idquan)
             ->update($lichlamviec);
         return back();
     }
-    public function editlichlamviec(Request $request){
+    public function editlichlamviec(Request $request)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
-        
+
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
 
         $lichlamviec = DB::table('lichlamviec')
             ->orderBy('hoten')
-            ->where('lichlamviec.idquan',$thanhvien->idquan)
+            ->where('lichlamviec.idquan', $thanhvien->idquan)
             ->where('lichlamviec.idkhuvuc', $request->idkhuvuc)
             ->where('lichlamviec.idcalam', $request->idcalam)
             ->where('lichlamviec.thoigian', $request->thoigian)
-            ->join('thanhvien','lichlamviec.idthanhvien','=','thanhvien.id')
-            ->join('vaitro', 'thanhvien.idvaitro','=','vaitro.id')
-            ->select('lichlamviec.idthanhvien','thanhvien.hoten','thanhvien.namsinh','thanhvien.id','vaitro.tenvaitro')
+            ->join('thanhvien', 'lichlamviec.idthanhvien', '=', 'thanhvien.id')
+            ->join('vaitro', 'thanhvien.idvaitro', '=', 'vaitro.id')
+            ->select('lichlamviec.idthanhvien', 'thanhvien.hoten', 'thanhvien.namsinh', 'thanhvien.id', 'vaitro.tenvaitro')
             ->get();
-        
+
         $thanhvien2 = DB::table('thanhvien')
             ->orderBy('hoten')
-            ->where('thanhvien.idquan',$thanhvien->idquan)
-            ->where('hidden',0)
-            ->join('vaitro','thanhvien.idvaitro','=','vaitro.id')
-            ->select('thanhvien.*','vaitro.tenvaitro')
+            ->where('thanhvien.idquan', $thanhvien->idquan)
+            ->where('hidden', 0)
+            ->join('vaitro', 'thanhvien.idvaitro', '=', 'vaitro.id')
+            ->select('thanhvien.*', 'vaitro.tenvaitro')
             ->get();
-        
+
         $thoigian = $request->thoigian;
         $idkhuvuc = $request->idkhuvuc;
         $idcalam = $request->idcalam;
-        return view('lichlamviec.editlichlamviec',compact('thanhvien','lichlamviec','thanhvien2','thoigian','idkhuvuc','idcalam'));
+        return view('lichlamviec.editlichlamviec', compact('thanhvien', 'lichlamviec', 'thanhvien2', 'thoigian', 'idkhuvuc', 'idcalam'));
     }
-    public function addlichlamviec(Request $request){
+    public function addlichlamviec(Request $request)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
 
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
 
-        
+
         $thanhvien2 = $request->input('idthanhvien');
-        foreach ($thanhvien2 as $key => $row){
+        foreach ($thanhvien2 as $key => $row) {
             $lichlamviec['thoigian'] = $request->thoigian;
             $lichlamviec['idquan'] = $thanhvien->idquan;
             $lichlamviec['idkhuvuc'] = $request->idkhuvuc;
@@ -149,63 +156,78 @@ class quanlylichlamviecController extends Controller
                 ->insert($lichlamviec);
         }
         $thoigian = $request->thoigian;
-        return redirect()->route('xemlichlamviec',compact('thoigian'));
+        return redirect()->route('xemlichlamviec', compact('thoigian'));
     }
-    public function changelichlamviec(Request $request){
+    public function changelichlamviec(Request $request)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
 
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
 
         $thanhvien2 = $request->input('idthanhvien');
         $lichlamviec3 = DB::table('lichlamviec')
-            ->where('idquan',$thanhvien->idquan)
+            ->where('idquan', $thanhvien->idquan)
             ->where('idcalam', $request->idcalam)
-            ->where('idkhuvuc',$request->idkhuvuc)
-            ->where('thoigian',$request->thoigian)
+            ->where('idkhuvuc', $request->idkhuvuc)
+            ->where('thoigian', $request->thoigian)
             ->get();
-        foreach ($lichlamviec3 as $key2 => $row2){
-            if($thanhvien2!=null){
-                foreach ($thanhvien2 as $key => $row){
+        foreach ($lichlamviec3 as $key2 => $row2) {
+            if ($thanhvien2 != null) {
+                foreach ($thanhvien2 as $key => $row) {
                     DB::table('lichlamviec')
-                        ->where('idquan',$thanhvien->idquan)
+                        ->where('idquan', $thanhvien->idquan)
                         ->where('idcalam', $request->idcalam)
-                        ->where('idkhuvuc',$request->idkhuvuc)
-                        ->where('thoigian',$request->thoigian)
-                        ->where('idthanhvien',$row)
+                        ->where('idkhuvuc', $request->idkhuvuc)
+                        ->where('thoigian', $request->thoigian)
+                        ->where('idthanhvien', $row)
                         ->delete();
                 }
             }
         }
         $thoigian = $request->thoigian;
-        return redirect()->route('xemlichlamviec',compact('thoigian'));
+        return redirect()->route('xemlichlamviec', compact('thoigian'));
     }
-    public function copylichlamviec(Request $request){
+    public function copylichlamviec(Request $request)
+    {
         $ssidthanhvien = Session::get('ssidthanhvien');
 
         $thanhvien = DB::table('thanhvien')
-            ->where('thanhvien.id',$ssidthanhvien)
-            ->join('users','thanhvien.idquan','=','users.id')
-            ->select('thanhvien.*','users.hinhquan','users.name')
+            ->where('thanhvien.id', $ssidthanhvien)
+            ->join('users', 'thanhvien.idquan', '=', 'users.id')
+            ->select('thanhvien.*', 'users.hinhquan', 'users.name')
             ->first();
+
         $tungay = $request->tungay;
         $sangngay = $request->sangngay;
-        $lichlamviec = DB::table('lichlamviec')
-            ->where('idquan',$thanhvien->idquan)
-            ->where('thoigian',$tungay)
-            ->get();
-       foreach ($lichlamviec as $key => $row){
-           $lichlamviec2['idquan'] = $row->idquan;
-           $lichlamviec2['idkhuvuc'] = $row->idkhuvuc;
-           $lichlamviec2['idcalam'] = $row->idcalam;
-           $lichlamviec2['idthanhvien'] = $row->idthanhvien;
-           $lichlamviec2['thoigian'] = $sangngay;
-           $lichlamviec2['diemdanh'] = 0;
-           DB::table('lichlamviec')->insert($lichlamviec2);
-       }
-       return back();
+        if ($sangngay <= $tungay) {
+            return back()->withErrors('Không thể thêm lịch cho quá khứ');
+        } else {
+            $check = DB::table('lichlamviec')
+                ->where('idquan', $thanhvien->idquan)
+                ->where('thoigian', $sangngay)
+                ->get();
+            if (count($check) != 0) {
+                return back()->withErrors('Ngày ' . $sangngay . ' ' . 'đã có lịch, không thể copy, mời bạn thêm thủ công.');
+            } else {
+                $lichlamviec = DB::table('lichlamviec')
+                    ->where('idquan', $thanhvien->idquan)
+                    ->where('thoigian', $tungay)
+                    ->get();
+                foreach ($lichlamviec as $key => $row) {
+                    $lichlamviec2['idquan'] = $row->idquan;
+                    $lichlamviec2['idkhuvuc'] = $row->idkhuvuc;
+                    $lichlamviec2['idcalam'] = $row->idcalam;
+                    $lichlamviec2['idthanhvien'] = $row->idthanhvien;
+                    $lichlamviec2['thoigian'] = $sangngay;
+                    $lichlamviec2['diemdanh'] = 0;
+                    DB::table('lichlamviec')->insert($lichlamviec2);
+                }
+                return back();
+            }
+        }
     }
 }
--- resources/views/lichlamviec/quanlylichlamviec.blade.php
+++ resources/views/lichlamviec/quanlylichlamviec.blade.php
@@ -1,61 +1,61 @@
 @extends('layouts.admin')
 
 @section('title')
-  <title>Quản lý lịch làm việc</title>
+<title>Quản lý lịch làm việc</title>
 @endsection
 @section('home')
-	<li class="nav-item d-none d-sm-inline-block">
-		<a href="{{route('thongtinthanhvien')}}" class="nav-link">Home</a>
-    </li>
+<li class="nav-item d-none d-sm-inline-block">
+    <a href="{{route('thongtinthanhvien')}}" class="nav-link">Home</a>
+</li>
 @endsection
 @section('dangxuat')
-	<ul class="navbar-nav ml-right">
-      	<li class="nav-item d-none d-sm-inline-block">
-        	<a href="{{route('dangxuatthanhvien')}}" class="nav-link">Đăng xuất</a>
-      	</li>
-    </ul>
+<ul class="navbar-nav ml-right">
+    <li class="nav-item d-none d-sm-inline-block">
+        <a href="{{route('dangxuatthanhvien')}}" class="nav-link">Đăng xuất</a>
+    </li>
+</ul>
 @endsection
 @section('quan')
-	<a href="{{route('login')}}" class="brand-link">
-  		<img src="{{$thanhvien->hinhquan}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      	<span class="brand-text font-weight-light">{{$thanhvien->name}}</span>
-	</a>
+<a href="{{route('login')}}" class="brand-link">
+    <img src="{{$thanhvien->hinhquan}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">{{$thanhvien->name}}</span>
+</a>
 @endsection
 @section('avatar')
-	<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          	<img src="{{$thanhvien->hinhtv}}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          	<a href="{{route('thongtinthanhvien')}}" class="d-block">{{$thanhvien->hoten}}</a>
-        </div>
+<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="image">
+        <img src="{{$thanhvien->hinhtv}}" class="img-circle elevation-2" alt="User Image">
     </div>
+    <div class="info">
+        <a href="{{route('thongtinthanhvien')}}" class="d-block">{{$thanhvien->hoten}}</a>
+    </div>
+</div>
 @endsection
 
 @section('content')
-      <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Quản lý lịch làm việc</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{route('thongtinthanhvien')}}">Thông tin thành viên</a></li>
-                <li class="breadcrumb-item"><a href="">Quản lý lịch làm việc</a></li>
-                </ol>
-            </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <h1 class="m-0">Quản lý lịch làm việc</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{route('thongtinthanhvien')}}">Thông tin thành viên</a></li>
+                        <li class="breadcrumb-item"><a href="">Quản lý lịch làm việc</a></li>
+                    </ol>
+                </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
     <!-- Main content -->
     <div class="content">
-      <div class="container-fluid">
-        <div class="row">
+        <div class="container-fluid">
+            <div class="row">
 
                 <div class="col-md-6">
                     <h4>Lịch làm việc của ngày:</h4>
@@ -64,7 +64,7 @@
                         {{csrf_field()}}
                         <div class="form-group">
                             <label>Ngày: </label>
-                            <input type="date" name="thoigian" value="{{$date}}"required>
+                            <input type="date" name="thoigian" value="{{$date}}" required>
                             <button type="submit" class="btn btn-primary">Kiểm tra</button>
                         </div>
                     </form>
@@ -76,57 +76,64 @@
                         {{csrf_field()}}
                         <div class="form-group">
                             <label>Từ ngày: </label>
-                            <input type="date" name="tungay" value="{{$date}}"required>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="date" name="tungay" value="{{$date}}" required>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <label>Sang ngày: </label>
-                            <input type="date" name="sangngay" value=""required>
+                            <input type="date" name="sangngay" value="" required>
                             <button type="submit" class="btn btn-primary">Copy</button>
                         </div>
                     </form>
+                    @if($errors->any())
+                    <h3 style="color:red">{{$errors->first()}}</h3>
+                    @endif
                 </div>
                 <div class="col-md-12">
-                <table class="table table-bordered text-center">
-                    <thead>
-                        <tr>
-                            <th scope="col">CA LÀM / KHU VỰC</th>
-                            @foreach ($khuvuc as $key => $row)
-                            <th scope="col">{{$row->tenkhuvuc}}</th>
-                            @endforeach
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($calam as $key2 => $row2)
-                        <tr>
-                            <th scope="row">{{$row2->tencalam}}<br>{{$row2->tu}}->{{$row2->den}}</th>
-                            @foreach ($khuvuc as $key => $row)
-                            <td>
-                                <form action="{{route('editlichlamviec')}}" method="get">
-                                    {{csrf_field()}}
-                                    <input type="hidden" name="thoigian" value="{{$date}}">
-                                    <input type="hidden" name="idkhuvuc" value="{{$row->id}}">
-                                    <input type="hidden" name="idcalam" value="{{$row2->id}}">
-                                    <button class="btn btn-warning" type="submit"><i class="fas fa-edit"></i>Sửa</button>
-                                </form>
-                                @foreach ($lichlamviec as $key3 => $row3)
-                                    @if($row3->idkhuvuc == $row->id && $row3->idcalam == $row2->id)
-                                        @if($row3->diemdanh==0)
-                                            <a style="color:red" href="{{route('diemdanhcomatlichlamviec',['id'=>$row3->id])}}">{{$row3->hoten}}--->Vắng mặt </a><br>
-                                        @else
-                                            <a style="color:green" href="{{route('diemdanhvangmatlichlamviec',['id'=>$row3->id])}}">{{$row3->hoten}}--->Có mặt </a><br>
-                                        @endif
-                                    @endif
+                    <table class="table table-bordered text-center">
+                        <thead>
+                            <tr>
+                                <th scope="col">CA LÀM / KHU VỰC</th>
+                                @foreach ($khuvuc as $key => $row)
+                                <th scope="col">{{$row->tenkhuvuc}}</th>
                                 @endforeach
-                            </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($calam as $key2 => $row2)
+                            <tr>
+                                <th scope="row">{{$row2->tencalam}}<br>{{$row2->tu}}->{{$row2->den}}</th>
+                                @foreach ($khuvuc as $key => $row)
+                                <td>
+                                    <form action="{{route('editlichlamviec')}}" method="get">
+                                        {{csrf_field()}}
+                                        <input type="hidden" name="thoigian" value="{{$date}}">
+                                        <input type="hidden" name="idkhuvuc" value="{{$row->id}}">
+                                        <input type="hidden" name="idcalam" value="{{$row2->id}}">
+                                        <button class="btn btn-warning" type="submit"><i class="fas fa-edit"></i>Sửa</button>
+                                    </form>
+                                    @foreach ($lichlamviec as $key3 => $row3)
+                                    @if($row3->idkhuvuc == $row->id && $row3->idcalam == $row2->id)
+                                    @if($row3->diemdanh==0)
+                                    <button type="button" class="btn btn-danger">
+                                        <a style="color:black" href="{{route('diemdanhcomatlichlamviec',['id'=>$row3->id])}}">{{$row3->hoten}}--->Chưa điểm danh</a>
+                                    </button><br>
+                                    @else
+                                    <button type="button" class="btn btn-success">
+                                        <a style="color:black" href="{{route('diemdanhvangmatlichlamviec',['id'=>$row3->id])}}">{{$row3->hoten}}--->Đã điểm danh</a>
+                                    </button><br>
+                                    @endif
+                                    @endif
+                                    @endforeach
+                                </td>
+                                @endforeach
+                            </tr>
                             @endforeach
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
                 </div>
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+<!-- /.content-wrapper -->
 @endsection
